157500daa9db94c334b0719c6c22504e8787a36e
Rik Lomas <lomas.rik@gmail.com>
2017-12-09 16:12:27 -0500

    force login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,5 +21,12 @@ class ApplicationController < ActionController::Base
     session[:user_id].present?
   end
   
+  def force_login
+    unless is_logged_in?
+      flash[:error] = "You are not logged in"
+      redirect_to root_path
+    end
+  end 
+
   
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   
+  before_action :force_login
+  
   def index
     @items = Item.all
   end
