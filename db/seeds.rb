17e791182bbb414d66772cf32001865396b82a3a
Rik Lomas <lomas.rik@gmail.com>
2017-12-09 14:54:49 -0500

    added active admin panels

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+AdminUser.create!(email: 'rik@superhi.com', password: 'test123', password_confirmation: 'test123')
