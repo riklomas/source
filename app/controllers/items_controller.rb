d5d8b21185d047a384f7025d4d6e5ff26633f00a
Rik Lomas <lomas.rik@gmail.com>
2017-12-16 18:15:55 -0500

    added search to items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,16 @@ class ItemsController < ApplicationController
   before_action :force_login
   
   def index
-    @items = Item.all
+    # if there's a search, filter!!!
+    # if not, show all!
+    
+    if params[:q].present?
+      # this is where we search!
+      @items = Item.where("lower(title) LIKE ?", "%" + params[:q].downcase + "%")
+    else
+      # show all
+      @items = Item.all
+    end
   end
   
   def show
