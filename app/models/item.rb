7f0185c65881aabda3c274e8159087c32348a625
Rik Lomas <lomas.rik@gmail.com>
2017-12-16 17:39:07 -0500

    added categories to items

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,8 @@
 class Item < ApplicationRecord
   
+  has_many :category_items
+  has_many :categories, through: :category_items
+  
   validates :title, presence: true
   
   mount_uploader :image, ImageUploader
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171216210756) do
+ActiveRecord::Schema.define(version: 20171216212936) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -47,6 +47,15 @@ ActiveRecord::Schema.define(version: 20171216210756) do
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "category_items", force: :cascade do |t|
+    t.integer "item_id"
+    t.integer "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_category_items_on_category_id"
+    t.index ["item_id"], name: "index_category_items_on_item_id"
   end
 
   create_table "items", force: :cascade do |t|
