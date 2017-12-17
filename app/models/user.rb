206c0dc5aaa6839bae757c0996c384d26d84a389
Rik Lomas <lomas.rik@gmail.com>
2017-12-17 18:59:02 -0500

    Added user tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
   validates :stripe_token, presence: true
+  validates :subscription_plan, inclusion: { in: ["basic", "pro"] }
   
   has_secure_password
   
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,34 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save when the user is blank" do
+    assert_equal User.new.save, false
+  end
+  
+  test "should save if user is full" do
+    @user = users(:rik)
+    
+    assert_equal @user.save, true
+  end
+  
+  test "should save and subscribe" do
+    @user = users(:rik)
+    
+    assert_equal @user.save_and_subscribe, true
+  end
+  
+  test "should not save and subscribe with invalid plan" do
+    @user = users(:rik)
+    @user.subscription_plan = "fakeplan"
+    
+    assert_equal @user.save_and_subscribe, false
+  end
+  
+  test "should not charge if card declined" do
+    @user = users(:milan)
+    
+    assert_equal @user.save_and_subscribe, false
+    assert_equal @user.errors[:stripe_token], ["Your card was declined."]
+  end
+  
 end
